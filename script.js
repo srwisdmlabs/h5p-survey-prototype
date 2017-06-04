48fa5a9bb446ce5eb9dcd723076372ffff8ed5a5
Satej <satej.rajam@inscripts.in>
2017-06-04 19:43:19 +0530

    added new files

---
+++ script.js
@@ -0,0 +1,8 @@
+// <div class="question-container">
+// 						<h2>Question goes here</h2>
+// 					</div>
+// 					<div class="options-container">
+// 						<div>
+// 							<p>ptions goes here</p>
+// 						</div>
+// 					</div>
